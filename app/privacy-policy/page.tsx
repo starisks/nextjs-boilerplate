bf10c2067ed758a72ecb9b293af9da4b7d755a75
starisks <butter.the.bread727@gmail.com>
2026-03-22 13:53:40 +0700

    Create page.tsx

---
+++ app/privacy-policy/page.tsx
@@ -0,0 +1,18 @@
+export const metadata = {
+  title: "How to Solve a Rubik's Cube",
+  description: "Step-by-step beginner guide.",
+};
+
+export default function Blog(){
+  return (
+    <main className="p-10 max-w-3xl mx-auto">
+      <h1 className="text-4xl mb-4">
+        How to Solve a Rubik's Cube
+      </h1>
+
+      <p>
+        This guide teaches you the beginner method to solve the cube...
+      </p>
+    </main>
+  );
+}
